ad92e7575d50679826aea155f6427817acbce9f7
shuya.tasaka <s.tasaka91@gmail.com>
2022-02-05 10:44:29 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,10 +9,16 @@ class ImageService
 {
   public static function upload($imageFile, $folderName)
   {
+    if(is_array($imageFile)) {
+      $file = $imageFile['image'];
+    } else {
+      $file = $imageFile;
+    }
+
     $fileName = uniqid(rand() . '_');
-    $extention = $imageFile->extension();
+    $extention = $file->extension();
     $fileNameToStore = $fileName . '.' . $extention;  
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+    $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
 
     Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
 
